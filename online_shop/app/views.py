5021a8982a4bda09edf6033de85a89a82a37ba0a
Satyam Mishra <satyam.mishra@incedoinc.com>
2021-04-01 16:24:24 +0530

    added few dummy test cases

--- online_shop/app/views.py
+++ online_shop/app/views.py
@@ -6,6 +6,8 @@ from django.template import loader
 def home(request):
     template = loader.get_template('home.html')
     context = {
-        'name':"Satyam Mishra"
+        'name':"Satyam Mishra",
+        "email": "satyam@gmail.com",
+        "address": "Bangalore",
     }
     return HttpResponse(template.render(context, request))
